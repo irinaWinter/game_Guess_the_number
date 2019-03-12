41ca6bdb26880bacd86be3e5001ae962137b19e5
irinaWinter <lobanova.irina.96@mail.ru>
2019-03-12 15:59:36 +0300

    Изменение диапазона значений

--- game.js
+++ game.js
@@ -1,26 +1,31 @@
 var answer = Math.floor(Math.random() * 101);
-console.log(answer);
 var answerField = document.querySelector('.field__answer');
-// answerField.textContent = answer;
 var possibleAnswer = document.querySelector('.field__guess');
+var range = document.querySelector('.field__range');
+var min = 0;
+var max = 100;
+range.textContent = 'от ' + min + ' до ' + max;
+
 var button = document.querySelector('.field__button');
 button.onclick = function() {
 	var gameplay = function(event) {
 		if (Number((possibleAnswer.value), 10) === answer) {
 			answerField.textContent = answer;
 			possibleAnswer.value = '';
+
 		} else if (Number((possibleAnswer.value), 10) > answer) {
 			answerField.textContent = 'Меньше';
+			max = Number((possibleAnswer.value), 10)
 			possibleAnswer.value = '';
 		} else {
 			answerField.textContent = 'Больше';
+			min = Number((possibleAnswer.value), 10)
 			possibleAnswer.value = '';
-		}
-		if (event.keyCode == 13) alert('Привет');
-		addEventListener("keydown", gameplay);
-
+		}	
 	};
+
 	gameplay();
+	range.textContent = 'от ' + min + ' до ' + max;
 
 
 }
